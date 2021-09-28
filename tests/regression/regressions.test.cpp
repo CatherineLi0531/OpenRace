6020aeb114eb5e21c3b82e1e1e7332a86470f18b
BradSwain <brad@coderrect.com>
2021-09-28 13:50:06 -0500

    Record volatile (#333)
    * record volatile read/writes
    
    * simplify test makefile

--- tests/regression/regressions.test.cpp
+++ tests/regression/regressions.test.cpp
@@ -14,6 +14,7 @@ limitations under the License.
 #include <catch2/catch.hpp>
 
 #include "Analysis/HappensBeforeGraph.h"
+#include "Trace/Event.h"
 
 TEST_CASE("No crash on empty thread", "[regression][happensbefore]") {
   // Previously a bug caused HappensBefore to crash if a spawned thread contained no events
@@ -47,4 +48,49 @@ declare i32 @pthread_join(i64, i8**)
   race::ProgramTrace program(module.get());
 
   race::HappensBeforeGraph happensbefore(program);
+}
+
+TEST_CASE("Record volatile load/stores", "[regression][ir]") {
+  // Volatile cannot be used to prevent races in C++
+  // Volatile accesses should be considered for race detection
+  // See https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rconc-volatile
+
+  const char *ModuleString = R"(
+%union.pthread_attr_t = type { i64, [48 x i8] }
+
+@global = common global i32 0
+
+define i8* @entry(i8* %_arg) {
+entry:
+  store volatile i32 1, i32* @global
+  store volatile i32 2, i32* @global
+  ret i8* null
+}
+
+define void @main() {
+  %t = alloca i64
+  %call = call i32 @pthread_create(i64* nonnull %t, %union.pthread_attr_t* null, i8* (i8*)* nonnull @entry, i8* null)
+  %1 = load i64, i64* %t
+  %call1 = call i32 @pthread_join(i64 %1, i8** null)
+  ret void
+}
+
+declare i32 @pthread_create(i64*, %union.pthread_attr_t*, i8* (i8*)*, i8*)
+declare i32 @pthread_join(i64, i8**)
+)";
+
+  llvm::LLVMContext Ctx;
+  llvm::SMDiagnostic Err;
+  auto module = llvm::parseAssemblyString(ModuleString, Err, Ctx);
+  if (!module) {
+    Err.print("error", llvm::errs());
+  }
+
+  race::ProgramTrace program(module.get());
+
+  REQUIRE(program.getThreads().size() == 2);
+
+  auto const &thread = program.getThreads().at(1);
+  REQUIRE(thread->getEvents().size() == 2);
+  CHECK(thread->getEvent(0)->type == race::Event::Type::Write);
 }
